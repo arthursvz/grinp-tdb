93982966a619f851f0a901a4f9b5be2d4d7b9d2b
Arthur SAUVEZIE <65153093+arthursvz@users.noreply.github.com>
2026-01-19 17:09:03 +0100

    Add files via upload

---
+++ src/lib/index.ts
@@ -0,0 +1,32 @@
+// place files you want to import through the `$lib` alias in this folder.
+
+import { z } from "zod";
+
+export const loginScheme = z.object({
+    email: z.string().email(),
+    password: z.string().min(8),
+});
+
+export const registerScheme = z.object({
+    first_name: z.string().min(1),
+    last_name: z.string().min(1),
+    email: z.string().email(),
+    password: z.string().min(8),
+    confirmPassword: z.string().min(8)
+}).refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ["confirmPassword"],
+});
+
+// TODO : add date range check
+export const slotScheme = z.object({
+    title: z.string().min(5),
+    description: z.string().min(5),
+    capacity: z.number().int().positive(),
+    date: z.object({
+        starts_at: z.string().min(1).regex(/^[0-9]{2}:[0-9]{2}$/),//z.date(),
+        ends_at: z.string().min(1).regex(/^[0-9]{2}:[0-9]{2}$/),//z.date(),
+    })
+});
+
+export type FormSchema = typeof loginScheme;
---
+++ src/lib/server/lucia.ts
@@ -0,0 +1,60 @@
+// src/lib/server/lucia.ts
+
+import { env } from "$env/dynamic/private";
+import prisma from "$lib/server/prisma";
+import { PrismaAdapter } from "@lucia-auth/adapter-prisma";
+import { Authentik } from "arctic";
+import { Lucia } from "lucia";
+
+const realmURL = 'https://auth.inpt.fr';
+
+export const authentik = new Authentik(
+    realmURL,
+    env.CHURROS_CLIENT_ID,
+    env.CHURROS_CLIENT_SECRET,
+    env.REDIRECT_URI,
+);
+
+const adapter = new PrismaAdapter(prisma.session, prisma.user);
+export const lucia = new Lucia(adapter, {
+    sessionCookie: {
+        attributes: {
+            secure: !env.DEV,
+            sameSite: "lax",
+        }
+    },
+    getUserAttributes: (attributes) => {
+        return {
+            churros_id: attributes.churros_id,
+            email: attributes.email,
+            root: attributes.root,
+            instructor: attributes.instructor
+        };
+    },
+    getSessionAttributes: (attributes) => {
+        return {
+            userId: attributes.userId,
+            expiresAt: attributes.expiresAt
+        }
+    }
+});
+
+declare module "lucia" {
+    interface Register {
+        Lucia: typeof Lucia;
+        DatabaseUserAttributes: DatabaseUserAttributes;
+        DatabaseSessionAttributes: DatabaseSessionAttributes;
+    }
+}
+
+interface DatabaseUserAttributes {
+    email: string;
+    churros_id: string;
+    root: boolean;
+    instructor: boolean;
+}
+
+interface DatabaseSessionAttributes {
+    userId: string;
+    expiresAt: Date;
+}
